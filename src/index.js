b4f5803bcfc5f2d11aaf3dc8abf2064dfb22ce49
maosan132 <maosan132@gmail.com>
2021-08-02 14:24:41 -0500

    Deleted constructor in square constructor 'cause we are no longer use state here

--- src/index.js
+++ src/index.js
@@ -14,9 +14,9 @@ class Square extends React.Component {
     return (
       <button
         className='square'
-        onClick={() => this.setState({value: 'X'})}
+        onClick={() => this.props.onCLick()}
         >
-        {this.state.value}
+        {this.props.value}
       </button>
     )
   }
@@ -31,7 +31,12 @@ class Board extends React.Component {
   }
 
   renderSquare(i) {
-    return <Square value={this.state.squares[i]}/>
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick = {() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
