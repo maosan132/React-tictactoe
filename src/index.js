8af0b6cd9e8eb17ec619c80120c574579b466175
maosan132 <maosan132@gmail.com>
2021-08-02 17:18:16 -0500

    done the x - o alternancy

--- src/index.js
+++ src/index.js
@@ -40,8 +40,12 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    // squares[i] = 'X';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
