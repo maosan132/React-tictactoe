4d878d6a52a25eb332c19cdac1728b9dc965444b
maosan132 <maosan132@gmail.com>
2021-08-02 17:50:08 -0500

    Changed calculatewinner to old named function, because with arrow function dodn't work

--- src/index.js
+++ src/index.js
@@ -40,6 +40,9 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     // squares[i] = 'X';
     this.setState({
@@ -58,7 +61,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = `Next player: ${(this.state.xIsNext ? 'X' : 'O')}`;
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = `Winner: ${winner}`
+    } else {
+      status = `Next player: ${(this.state.xIsNext ? 'X' : 'O')}`;
+    }
 
     return (
       <div>
@@ -105,7 +114,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-const calculateWinner = squares => {
+/*const calculateWinner = squares => {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -117,10 +126,31 @@ const calculateWinner = squares => {
     [2, 4, 6],
   ];
   lines.forEach(i => {
-    const [a,b,c] = lines[i];
+    const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
   });
+  return null;
+}*/
+
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
   return null;
 }
