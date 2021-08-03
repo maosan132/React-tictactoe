9ea17ed7a2804a795e061b1386c141cdefb73f89
maosan132 <maosan132@gmail.com>
2021-08-02 22:33:50 -0500

    the tic-tac-toe board should immediately update to show what the board looked like after that step occurred

--- src/index.js
+++ src/index.js
@@ -91,11 +91,12 @@ class Game extends React.Component {
         squares : Array(9).fill(''),
       }],
       xIsNext: true,
+      stepNumber: 0,
     }
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -107,12 +108,20 @@ class Game extends React.Component {
         squares : squares,
       }]),
       xIsNext: !this.state.xIsNext,
+      stepNumber: history.length,
     });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    })
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
     const moves = history.map((step, move) => {
       const desc = move ?
